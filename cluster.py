4ff2bf56a19fc2ebdb632ee4f2a530f1fdfcb164
20201041010 <107189052+20201041010@users.noreply.github.com>
2022-12-26 19:58:48 +0800

    Update cluster.py

--- cluster.py
+++ cluster.py
@@ -10,6 +10,17 @@ def cluster():
     true_k = 3
     model = KMeans(n_clusters=true_k, init='k-means++', max_iter=100, n_init=1)
     model.fit(X)
+    
+    print("类中的高频关键词:")
+    order_centroids = model.cluster_centers_.argsort()[:, ::-1]
+    terms = vectorizer.get_feature_names()
+    for i in range(true_k):
+        print("聚类%d:" % i)
+        for ind in order_centroids[i, :10]:
+            print (' %s' % terms[ind])
+        print
+    
+    save(model)
 
 def save(model):
     #将结果保存
