043ca2dcd7a716fa43ddcb6c3bd9f2c3107b78cb
20201061175唐浩然 <79008579+20201061175@users.noreply.github.com>
2022-12-30 13:21:39 +0800

    添加matplotlib展示功能

--- word_cloud.py
+++ word_cloud.py
@@ -1,6 +1,8 @@
 import csv
 import wordcloud
 from cv2 import imread
+from PIL import Image
+import matplotlib.pyplot as plt
 
 mask = imread("background.png")#导入自定义词云图片
 
@@ -13,6 +15,13 @@ with open('cluster2.csv', 'r', encoding='utf-8', newline='') as f:
 w = wordcloud.WordCloud(scale=2,font_path="msyh.ttc",width=1000,height=700,background_color="white",mask=mask)
 w.generate(items)
 w.to_file("output2.png")#输出词云
+img = Image.open("output2.png")
+
+plt.figure("Image") # 图像窗口名称
+plt.imshow(img)
+plt.axis('on') # 关掉坐标轴为 off
+plt.title('image') # 图像题目
+plt.show()
 
 #对csv文件进行数据分析，生成词云
 
